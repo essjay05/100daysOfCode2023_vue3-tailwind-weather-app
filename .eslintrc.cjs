846e3a15f535949a5b75bbd688f23b34459e9f49
Joy Serquiña <stepjoyous@gmail.com>
2023-02-25 13:19:28 -0800

    Installed axios for api calls. Made some updates to fix module not being read and process.env not being read.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -10,5 +13,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  define: {
+    'process.env': {}
   }
 }
